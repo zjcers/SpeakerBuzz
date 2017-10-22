e824722533acf8f93cfdb51dcf739e1e1cb7e28c
Zane Cersovsky <zcersovsky@gmail.com>
2017-10-22 14:50:14 -0500

    Added the ability to enable to needed hardware in the SPI driver

--- Drivers/SPIMaster.c
+++ Drivers/SPIMaster.c
@@ -10,6 +10,7 @@
 #include <stdbool.h>
 #include "Packages/driverlib/inc/hw_memmap.h"
 #include "Packages/driverlib/ssi.h"
+#include "Packages/driverlib/gpio.h"
 #include "Packages/driverlib/sysctl.h"
 #include "Drivers/SPIMaster.h"
 
@@ -21,7 +22,18 @@ void EnablePeriphBlocking(uint32_t periph)
 struct SSI_config {
     uint32_t SSI_base_addr;
     uint32_t periph;
+    void (*initGPIO)(void);
 };
+static void initGPIOSSI3(void)
+{
+    EnablePeriphBlocking(SYSCTL_PERIPH_GPIOQ);
+    GPIOPinConfigure(GPIO_PQ0_SSI3CLK);
+    GPIOPinConfigure(GPIO_PQ1_SSI3FSS);
+    GPIOPinConfigure(GPIO_PQ2_SSI3XDAT0);
+    GPIOPinConfigure(GPIO_PQ3_SSI3XDAT1);
+    GPIOPinTypeSSI(GPIO_PORTQ_BASE, GPIO_PIN_0 | GPIO_PIN_1 | GPIO_PIN_2 | GPIO_PIN_3);
+}
+//TODO: Finish writing the GPIO init functions so that the other SSI devices can actually be used
 const struct SSI_config SSI_configs[] = {
     {
         .SSI_base_addr = SSI0_BASE,
@@ -37,7 +49,8 @@ const struct SSI_config SSI_configs[] = {
     },
     {
         .SSI_base_addr = SSI3_BASE,
-        .periph = SYSCTL_PERIPH_SSI3
+        .periph = SYSCTL_PERIPH_SSI3,
+        .initGPIO = &initGPIOSSI3
     }
 };
 void SPIMInit(struct SPIInstance* inst, enum SPIDevice dev, enum SPIProtocol protocol)
@@ -45,6 +58,7 @@ void SPIMInit(struct SPIInstance* inst, enum SPIDevice dev, enum SPIProtocol pro
     inst->base_addr = (void*)(SSI_configs[dev].SSI_base_addr);
     inst->protocol = protocol;
     EnablePeriphBlocking(SSI_configs[dev].periph);
+    SSI_configs[dev].initGPIO();
     SSIConfigSetExpClk(SSI_configs[dev].SSI_base_addr,
                       system_clock,
                       protocol,
@@ -54,9 +68,9 @@ void SPIMInit(struct SPIInstance* inst, enum SPIDevice dev, enum SPIProtocol pro
     SSIEnable(SSI_configs[dev].SSI_base_addr);
 }
 
-void SPIMWrite(struct SPIInstance* inst, uint16_t* buf, size_t n)
+void SPIMWriteWord(struct SPIInstance* inst, uint16_t word)
 {
     uint32_t base = (uint32_t)(inst->base_addr);
-    for(size_t i = 0; i < n; i++)
-        SSIDataPut(base, ((char*)buf)[i]);
+    SSIDataPut(base, word);
+    SSIDataGet(base, &word); //Needed to make sure the receive FIFO does not fill up
 }
