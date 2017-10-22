64a9e852b34e1adad260a35e6c3597389b438e7f
Zane Cersovsky <zcersovsky@gmail.com>
2017-10-21 21:06:33 -0500

    Removed the manual handling of the frame select signal in the DAC driver

--- Drivers/DAC8311.c
+++ Drivers/DAC8311.c
@@ -7,16 +7,12 @@
 #include "DAC8311.h"
 #include "SPIMaster.h"
 
-void DAC8311Init(struct DAC8311Instance* dacInst, struct SPIInstance* spiInst, void (*select)(bool))
+void DAC8311Init(struct DAC8311Instance* dacInst, struct SPIInstance* spiInst)
 {
     dacInst->spim = spiInst;
-    dacInst->select = select;
-    select(true); // DAC8311 expects ~SYNC to be pulled HIGH by default
 }
 void DAC8311Write(struct DAC8311Instance* dacInst, uint16_t value)
 {
     value &= ~(0b11 << 14); //Clear the highest two bits to specify "normal operation" of the DAC
-    dacInst->select(false); //Set ~SYNC to LOW
     SPIMWrite(dacInst->spim, &value, 1); //Write value
-    dacInst->select(true); //Reassert ~SYNC
 }
--- Drivers/DAC8311.h
+++ Drivers/DAC8311.h
@@ -7,30 +7,16 @@
 
 #include "SPIMaster.h"
 
-/*
- * Example implementation of void select(bool enable):
- * (assuming the select line of the DAC was on port G, pin 2)
- * void DACSelect(bool enable)
- * {
- *     if (enable)
- *         GPIOPinWrite(GPIO_PORTG_BASE, GPIO_PIN_2, GPIO_PIN_2);
- *     else
- *         GPIOPinWrite(GPIO_PORTG_BASE, GPIO_PIN_2, 0x00);
- * }
- */
-
 struct DAC8311Instance {
     struct SPIInstance* spim; //The spi driver instance associated with this driver
-    void (*select)(bool); //A function to enable/disable the select line for the DAC
 };
 
 /*
  * Initializes the driver and hardware
  * @param dacInst - An uninitialized DAC8311Instance struct to keep driver state in
  * @param spiInst - A SPIMaster driver instance which has been initialized
- * @param select - A function that enables or disables the select line for the DAC
  */
-extern void DAC8311Init(struct DAC8311Instance* dacInst, struct SPIInstance* spiInst, void (*select)(bool));
+extern void DAC8311Init(struct DAC8311Instance* dacInst, struct SPIInstance* spiInst);
 
 /*
  * Writes a single value to the DAC
